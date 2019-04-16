312414270ce25b177513cf465da99793e82e6ff6
glitchCreativeLabs <34613727+GlitchLabs@users.noreply.github.com>
2019-04-16 17:41:52 -0400

    changes

--- module-4/week-6/day-1/index.js
+++ module-4/week-6/day-1/index.js
@@ -1,4 +1,59 @@
 // Change this file to do something else!
 
+
 const numberOfStates = 50;
 console.log(numberOfStates);
+
+const states =["Alabama",
+"Alaska", 
+"Arizona", 
+"Arkansas", 
+"California", 
+"Colorado", 
+"Connecticut", 
+"Delaware", 
+"Florida", 
+"Georgia", 
+"Hawaii", 
+"Idaho", 
+"Illinois",
+"Indiana", 
+"Iowa", 
+"Kansas", 
+"Kentucky", 
+"Louisiana", 
+"Maine", 
+"Maryland", 
+"Massachusetts", 
+"Michigan", 
+"Minnesota", 
+"Mississippi", 
+"Missouri", 
+"Montana", "Nebraska", 
+"Nevada", 
+"New Hampshire", 
+"New Jersey", 
+"New Mexico", 
+"New York", 
+"North Carolina", 
+"North Dakota", 
+"Ohio", 
+"Oklahoma", 
+"Oregon", 
+"Pennsylvania", "Rhode Island", 
+"South Carolina", 
+"South Dakota", 
+"Tennessee", 
+"Texas", 
+"Utah", 
+"Vermont", 
+"Virginia", 
+"Washington", 
+"West Virginia", 
+"Wisconsin", 
+"Wyoming"]
+var x = Math.floor((Math.random() * 50) + 1);
+var state = states[x];
+
+console.log("What state is number "+ x +" out of 50?");
+console.log(state);
